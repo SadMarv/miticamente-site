774ef9d7b7b19e60a8dc7a024d758dfdfe8283c7
Matheus <matheus.cmendessi@gmail.com>
2020-01-24 19:22:48 -0300

    update footer

--- src/footer-new.php
+++ src/footer-new.php
@@ -3,8 +3,7 @@
 <div class="container footer">
   <div class="row text-center">
     <div class="mx-auto">
-      <img src="<?php bloginfo('template_url'); ?>/static/images/cloudez-logo.png" class="cloudez-logo" alt="Cloudez">
-      <p class="copyright-cloudez">Cloudez é uma marca registrada Configr</p>
+      <p class="copyright-cloudez"></p>
     </div>
   </div>
 </div>
