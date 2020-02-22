366448afff0ea57cf85b1ac67a5fd68c9eccfce1
Matheus <matheus.cmendessi@gmail.com>
2020-02-22 05:39:53 -0300

    audio player structure

---
+++ src/scripts/home.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    $(".download").on('click', function(e){
+        // stopping form from reloading the page
+        e.preventDefault();
+        // serializing data from the target form
+        var podcastId = $(this).data('podcast-id');
+
+        console.log('podcast '+podcastId);
+      
+        $.ajax({
+          type: 'post',
+          dataType: 'text',
+          url: '/wp-admin/admin-ajax.php',
+          data: {
+            'action': 'contactAjax',
+            'data': podcastId
+          },
+          success: function(response){
+           var url = response.substring(0,(response.length - 1))
+            console.log('log '+url);
+          }
+        });
+      
+      }) // contactForm click event end
+});
+
