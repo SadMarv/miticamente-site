81dd4330097c19f4cbc6787ce68d133d8abd2310
Matheus <matheus.cmendessi@gmail.com>
2020-03-02 02:18:32 -0300

    del copys

--- single copy.php
+++
@@ -1,9 +0,0 @@
-<!doctype html5><html lang="pt-br"> <?php get_header('new'); ?> <body <?php body_class(); ?>> <?php get_header('navbar') ?> <?php
-      // Start the loop.
-      while ( have_posts() ) : the_post();
-      ?> <div class=""> <?php
-      the_content();
-      ?> </div> <?php
-      // End of the loop.
-      endwhile;
-    ?>  <?php get_footer('block') ?> </body> <?php wp_footer(); ?> <?php get_footer('scripts'); ?> </html>
